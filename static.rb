1c962c85805a2245854be3103636f77bb549e3c2
4RN4LDO <arnaldoporras@gmail.com>
2013-10-30 15:26:45 -0600

    process

--- static.rb
+++ static.rb
@@ -3,3 +3,9 @@ guess = gets.chomps
 
 puts "Can you guess the number I'm thinking?"
 
+ if guess > number
+	puts "Sorry, it's greater than that. Try again"
+else if guess < number
+	puts "Sorry, it's less than that. Try again"
+else
+	puts "Congrats!"
