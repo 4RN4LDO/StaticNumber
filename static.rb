01a0bb74ab4c1d4963741a3acd6dde564772e516
4RN4LDO <arnaldoporras@gmail.com>
2013-10-30 16:10:10 -0600

    process

--- static.rb
+++ static.rb
@@ -1,11 +1,15 @@
 number = 8
-guess = gets.chomps
 
 puts "Can you guess the number I'm thinking?"
 
- if guess > number
-	puts "Sorry, it's greater than that. Try again"
-else if guess < number
-	puts "Sorry, it's less than that. Try again"
-else
-	puts "Congrats!"
+print "Go ahead try!"
+guess = gets.chomp.to_i
+
+	if guess > number
+		puts "Sorry, it's greater than that. Try again"
+	else if guess < number
+			puts "Sorry, it's less than that. Try again"
+		else
+			puts "Congrats!"
+		end
+	end
