acf4c973d657048bb439e5f95b441fc86f176ce3
4RN4LDO <arnaldoporras@gmail.com>
2013-11-02 17:52:43 -0600

    Process

---
+++ random.rb
@@ -0,0 +1,17 @@
+number = 8
+
+loop do
+	print "Can you guess the number I'm thinking?"
+	guess = gets.chomp.to_i
+
+	unless guess == number
+		if guess > number
+			puts "Sorry, it's greater than that. Try again"
+		else 
+				puts "Sorry, it's less than that. Try again"
+		end
+	else
+		puts "Congrats!"
+		exit
+	end
+end
